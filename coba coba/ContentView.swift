5d9e00faf203228f145ffc0a7afefa6c49343796
Han <hansnrd@gmail.com>
2023-06-21 09:13:18 +0700

    Initial Commit

---
+++ coba coba/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  coba coba
+//
+//  Created by Han Sinaradi Budiman on 21/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ coba coba/coba_cobaApp.swift
@@ -0,0 +1,17 @@
+//
+//  coba_cobaApp.swift
+//  coba coba
+//
+//  Created by Han Sinaradi Budiman on 21/06/23.
+//
+
+import SwiftUI
+
+@main
+struct coba_cobaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
